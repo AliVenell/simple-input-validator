8cc31b72abf33097c94c5796a3fbc1824d6b397c
Ali Venell <ali.venell@gmail.com>
2015-12-22 16:05:11 +0200

    Added onPrepare browser maximize

--- conf.js
+++ conf.js
@@ -2,6 +2,11 @@ exports.config = {
 	//The address of a running selenium server.
   seleniumAddress: 'http://localhost:4444/wd/hub',
  // framework: 'jasmine2',
+  
+  onPrepare: function() {
+    browser.driver.manage().window().maximize();
+  },
+
  //Here we specify the name of the specs files.
   suites: {
   	healthcheck: 'spec.js'
