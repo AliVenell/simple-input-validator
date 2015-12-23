37a9175e7f9b00257b07fc50e0ec292dbbf6ff22
Ali Venell <ali.venell@gmail.com>
2015-12-23 13:44:01 +0200

    Added HtmlScreenshotReporter

--- conf.js
+++ conf.js
@@ -1,3 +1,5 @@
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
 exports.config = {
 	//The address of a running selenium server.
   seleniumAddress: 'http://localhost:4444/wd/hub',
@@ -12,12 +14,19 @@ exports.config = {
       savePath: 'testresults',
       filePrefix: 'reportXMLoutput'
     }));
+
+    jasmine.getEnv().addReporter(
+        new HtmlScreenshotReporter({
+          dest: 'screenshots',
+          filename: 'my-report.html',
+          captureOnlyFailedSpecs: true
+    }));
   },
 
  //Here we specify the name of the specs files.
   suites: {
-  	healthcheck: 'spec.js'
-    current: 'current_spec.js'
+  	healthcheck: 'spec.js',
+    current: 'spec.js'
   },
   
   jasmineNodeOpts: {
--- spec.js
+++ spec.js
@@ -7,8 +7,8 @@ describe('Login test', function() {
 
    it('Should login', function() {
     
-	browser.driver.get('https://www.test.posti.fi/omatpalvelut/seuranta/');
-   	expect(browser.driver.getTitle()).toEqual('Login page');
+	browser.driver.get('http://www.google.fi');
+   	expect(browser.driver.getTitle()).toEqual('Google');
   	browser.driver.findElement(by.name('login.username')).sendKeys('user1');
 	browser.driver.findElement(by.name('login.password')).sendKeys('passwd1');
 	browser.driver.findElement(by.id('login-button')).click();
